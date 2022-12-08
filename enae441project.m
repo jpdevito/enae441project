780503d34e8395ab4c3b6ed667ae8c6774e544d9
jpdevito <justindevito1@gmail.com>
2022-12-07 20:07:43 -0500

    Add to IOD section 3.1

--- enae441project.m
+++ enae441project.m
@@ -22,82 +22,87 @@ idxs = [2   3   4
         800 802 804
         840 841 842];
 
-setnum = 1;
+rv = []; % to keep track of calculated orbits
+for setnum = 1:height(idxs)
+    L = [];
+    R = [];
+    for obsnum = 1:3
+        i = idxs(setnum, obsnum);
+    
+        % Get ECI vector to observation site
+        lla_site.epoch = opt2satDset3.datetime(i);
+        Rvec = eci(lla_site).position_m';
+        
+        % Get AER to satellite from observation site
+        az = opt2satDset3.azimuth_deg(i);
+        el = opt2satDset3.elevation_deg(i);
+        aerhat = azelrn_deg(az, el, 1);
+        
+        % Get rhohat
+        aerhat.epoch = opt2satDset3.datetime(i);
+        eci_dir = eci(aerhat, lla_site).position_m';
+        rhohat = eci_dir - Rvec;
+        
+        L(:, obsnum) = rhohat;
+        R(:, obsnum) = Rvec;
+        dt(obsnum) = opt2satDset3.datetime(i);
+    end
+    
+    M = L\R;
+    
+    % Calculate a's
+    tau1 = seconds(dt(1) - dt(2));
+    tau3 = seconds(dt(3) - dt(2));
+    
+    % eq 7.83 in text
+    a1 = tau3/(tau3-tau1);
+    a3 = -tau1/(tau3-tau1);
+    a1u = tau3*((tau3-tau1)^2 - tau3^2) / (6*(tau3-tau1));
+    a3u = -tau1*((tau3-tau1)^2 - tau1^2) / (6*(tau3-tau1));
+    
+    % eq. 7.88 in text
+    A = M(2, 1)*a1 - M(2,2) + M(2, 3)*a3;
+    B = M(2, 1)*a1u + M(2, 3)*a3u;
+    
+    E = dot(L(:, 2), R(:, 2));
+    
+    mu = 3.986e14; % m^3/s^2
+    rE = 6378e3; % radius of the Earth, m
+    
+    % eq. 7.90
+    C8 = 1;
+    C6 = -(A^2 + 2*A*E + norm(R(:, 2))^2);
+    C3 = -2*mu*B*(A + E);
+    C0 = -mu^2*B^2;;
+    p = [C8 0 C6 0 0 C3 0 0 C0];
+    rts = roots(p);
+    
+    r2norm = rts(imag(rts) == 0 & rts > rE);
+    
+    u = mu / r2norm^3;
+    
+    % eq 7.101
+    c1 = a1 + a1u*u;
+    c2 = -1;
+    c3 = a3 + a3u*u;
+    
+    % eq. 7.102
+    csandrhos = M*[-c1; -c2; -c3];
+    
+    rho1 = csandrhos(1)/c1;
+    rho2 = csandrhos(2)/c2;
+    rho3 = csandrhos(3)/c3;
+    
+    r1 = R(:, 1) + rho1*L(:, 1);
+    r2 = R(:, 2) + rho2*L(:, 2);
+    r3 = R(:, 3) + rho3*L(:, 3);
+    
+    [v1, v2, v3] = gibbs(r1, r2, r3, mu);
 
-L = [];
-R = [];
-for obsnum = 1:3
-    i = idxs(setnum, obsnum);
-
-    % Get ECI vector to observation site
-    lla_site.epoch = opt2satDset3.datetime(i);
-    Rvec = eci(lla_site).position_m';
-    
-    % Get AER to satellite from observation site
-    az = opt2satDset3.azimuth_deg(i);
-    el = opt2satDset3.elevation_deg(i);
-    aerhat = azelrn_deg(az, el, 1);
-    
-    % Get rhohat
-    aerhat.epoch = opt2satDset3.datetime(i);
-    eci_dir = eci(aerhat, lla_site).position_m';
-    rhohat = eci_dir - Rvec;
-    
-    L(:, obsnum) = rhohat;
-    R(:, obsnum) = Rvec;
-    dt(obsnum) = opt2satDset3.datetime(i);
+    rv(:, end+1:end+3) = [r1 r2 r3 v1 v2 v3]';
 end
 
-M = L\R;
-
-% Calculate a's
-tau1 = seconds(dt(1) - dt(2));
-tau3 = seconds(dt(3) - dt(2));
-
-% eq 7.83 in text
-a1 = tau3/(tau3-tau1);
-a3 = -tau1/(tau3-tau1);
-a1u = tau3*((tau3-tau1)^2 - tau3^2) / (6*(tau3-tau1));
-a3u = -tau1*((tau3-tau1)^2 - tau1^2) / (6*(tau3-tau1));
-
-% eq. 7.88 in text
-A = M(2, 1)*a1 - M(2,2) + M(2, 3)*a3;
-B = M(2, 1)*a1u + M(2, 3)*a3u;
-
-E = dot(L(:, 2), R(:, 2));
-
-mu = 3.986e14; % m^3/s^2
-rE = 6378e3; % radius of the Earth, m
-
-% eq. 7.90
-C8 = 1;
-C6 = -(A^2 + 2*A*E + norm(R(:, 2))^2);
-C3 = -2*mu*B*(A + E);
-C0 = -mu^2*B^2;;
-p = [C8 0 C6 0 0 C3 0 0 C0];
-rts = roots(p);
-
-r2norm = rts(imag(rts) == 0 & rts > rE);
-
-u = mu / r2norm^3;
-
-% eq 7.101
-c1 = a1 + a1u*u;
-c2 = -1;
-c3 = a3 + a3u*u;
-
-% eq. 7.102
-csandrhos = M*[-c1; -c2; -c3];
-
-rho1 = csandrhos(1)/c1;
-rho2 = csandrhos(2)/c2;
-rho3 = csandrhos(3)/c3;
-
-r1 = R(:, 1) + rho1*L(:, 1);
-r2 = R(:, 2) + rho2*L(:, 2);
-r3 = R(:, 3) + rho3*L(:, 3);
-
-[v1, v2, v3] = gibbs(r1, r2, r3, mu);
+disp(rv)
 
 %% Functions
 
